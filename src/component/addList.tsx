5e01d77dcaa4f073ef70ee1c325a186e00fdccfd
unknown <adhamomarics@gmail.com>
2023-10-16 02:25:05 +0300

    chanege -style for icon

--- src/component/addList.tsx
+++ src/component/addList.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/actions";
-import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Form from "react-bootstrap/Form";
 
 const AddList: FC = () => {
   const [text, setText] = useState("");
@@ -18,18 +19,26 @@ const AddList: FC = () => {
 
   return (
     <div className="add-list-wrap">
-      <InputGroup className="mb-3">
-        <FormControl
-        className="new-task-input"
-          type="text"
-          placeholder="Enter a new todo"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <Button variant="outline-info" onClick={handleAdd}>
-          <FontAwesomeIcon icon={faPlus} /> 
-        </Button>
-      </InputGroup>
+      <Form>
+        <Form.Group className="mb-3">
+          <div className="new-task-wrap">
+            <input
+              className="new-task-input"
+              type="text"
+              placeholder=" New todo"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <Button
+              variant="outline-info"
+              onClick={handleAdd}
+              className="form-btn"
+            >
+              <FontAwesomeIcon icon={faPlus}  className="add-icon"/>
+            </Button>
+          </div>
+        </Form.Group>
+      </Form>
     </div>
   );
 };
