2058011fdc3ed6518531778532aedc95feb2514b
unknown <adhamomarics@gmail.com>
2023-10-17 18:49:37 +0300

    fix progress bar css

--- src/component/TodoProgressBar.tsx
+++ src/component/TodoProgressBar.tsx
@@ -9,11 +9,14 @@ const TodoProgressBar = ({ progress }: TodoProgressBarProps) => {
   if (isNaN(progress)) {
     progress = 0;
   }
+  const limitedProgress = Math.min(100, Math.max(0, progress));
+  
+  const formattedProgress = limitedProgress.toFixed(0);
   return (
     <div className="todo-progress-bar-container">
       <h2 className="todo-progress-label">Todo Done</h2>
       <div style={{ width: "100%", maxWidth: 150 }}>
-        <CircularProgressbar value={progress} text={`${progress}%`} />;
+        <CircularProgressbar value={progress} text={`${formattedProgress}%`} />;
       </div>
     </div>
   );
